718fb976df0f6544bec5efe4992f87a9691dc0c9
theoublalchararfeddine <theoublal@gmail.com>
2024-05-13 17:04:24 +0100

    make change

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,14 @@
-import Card from './Card.jsx'
+import List from './list.jsx'
+
+
 
 function App() {
   
   return (
-    <>
 
-         <Card 
-         name="theoublal"
-         age = {21}
-         AKA = "theoublal"
-         Placeofgrowth = "taroudant"
-         isWork = {true}
-         />
-      
-
-         <Card 
-         name=" charafe ddine "
-         age = {21}
-         AKA = "theWhileAlphaC"
-         Placeofgrowth = "TATA"
-         isWork = {false}
-         />
-         <Card 
-         name="theoublal"
-         age = {21}
-         AKA = "theoublal"
-         Placeofgrowth = "taroudant"
-         isWork = {true}
-         />
-      
-
-         <Card 
-         name=" charafe ddine "
-         age = {21}
-         AKA = "theWhileAlphaC"
-         Placeofgrowth = "TATA"
-         isWork = {false}
-         />
-         <Card 
-         name="theoublal"
-         age = {21}
-         AKA = "theoublal"
-         Placeofgrowth = "taroudant"
-         isWork = {true}
-         />
-      
-
-         <Card 
-         name=" charafe ddine "
-         age = {21}
-         AKA = "theWhileAlphaC"
-         Placeofgrowth = "TATA"
-         isWork = {false}
-         />
-
-      </>
-
+        <>
+            <List />
+        </>
   )
 }
 
